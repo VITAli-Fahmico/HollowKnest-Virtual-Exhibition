4f0b4e077cc39968d5b52b01bfc4988a0a843df0
nico <141409541+nichothenacho64@users.noreply.github.com>
2025-07-07 11:49:55 +0200

    Create full exhibition page dynamically
    Now, the exhibition page loads dynamically; however, the ability to switch between items does not exist yet. All texts need to be put in their respective folder depending on tone and competency

--- scripts/exhibition_script.js
+++ scripts/exhibition_script.js
@@ -1,19 +1,23 @@
-// ! Note for myself (Nico)
-// why use 
-// this waits until the HTML document has been fully loaded and parsed
-// (before images and other external resorces e.g. stylesheets loading)
-// then the function runs!
+const imageDirectory = "assets";
+const textsDirectory = "item_texts";
+const targetAreas = {
+	"short": "shortTextArea",
+	"medium": "mediumTextArea",
+	"long": "longTextArea"
+};
 
-// JS often tries to manipulate HTML elements (like setting innerHTML, or adding event listeners)
-// if the script runs before the page has been successfully loaded, then there may be issues...
 
-let items = []
-let narratives = []
-let currentSelection = []
-let currentNarrative = ""
-let currentValue = ""
-let currentSort = ""
-let areas = []
+let items = [];
+let narratives = [];
+let currentSelection = [];
+let currentNarrative = "";
+let currentValue = "";
+let currentTextDurations = [];
+let currentSort = "";
+let areas = [];
+
+let tone = "";
+let competency = "";
 
 // runs the function when ready (i.e. everything inside the brackets)
 
@@ -24,13 +28,18 @@ document.addEventListener("DOMContentLoaded", async function() {
 	.then(response => response.json())
 	.then(data => {	
 		items = data.items
-		let startWith = data.meta.startWith
+		let startWith = data.meta.startWith;
 		// let item = items[startWith]
 
-		narratives = data.meta.narratives
-		currentNarrative = data.meta.startNarrative
-		currentValue = data.meta.startValue
-		areas = data.areas
+		narratives = data.meta.narratives;
+		currentNarrative = data.meta.startNarrative;
+		currentValue = data.meta.startValue;
+		currentTextDurations = data.meta.startTextDurations;
+		areas = data.areas;
+
+		currentCompetency = data.meta.defaultCompetency;
+		currentTone = data.meta.defaultTone;
+
 		prepareNarratives(currentNarrative)
 	})
 });
@@ -44,12 +53,12 @@ document.addEventListener("DOMContentLoaded", async function() {
 
 function prepareNarratives(narrativeType) {
 	if (narrativeType === "Chronological") {
-		items.sort((itemA, itemB) => itemA.yearId - itemB.yearId);
+		currentSelection = items.sort((itemA, itemB) => itemA.yearId - itemB.yearId);
 
 	} else if (narrativeType === "Hollow Knight") {
-		items.sort((itemA, itemB) => {
-			const areaA = itemA.metadata["Associated Hollow Knight area"];
-			const areaB = itemB.metadata["Associated Hollow Knight area"];
+		currentSelection = items.sort((itemA, itemB) => {
+			const areaA = itemA.metadata.hollowKnightArea;
+			const areaB = itemB.metadata.hollowKnightArea;
 			const areaOrderA = areas[areaA].areaNumber;
 			const areaOrderB = areas[areaB].areaNumber;
 			return areaOrderA - areaOrderB;
@@ -57,16 +66,130 @@ function prepareNarratives(narrativeType) {
 	}; 
 
 	// ! not done here yet...
-	let index = currentSelection.findIndex(item => {item[narrativeType] == currentSort})
-	if (index == -1) {
-		index = 0
+	let itemIndex = currentSelection.findIndex(item => {item[narrativeType] == currentSort})
+	if (itemIndex == -1) {
+		itemIndex = 0
 	}; // test if it works....
-	// showInfo(index)
+	getItemInformation(itemIndex);
 }
 
-// function showInfo(index) {
+function getItemInformation(itemIndex) {
+	getItemImages(itemIndex);
+	getItemMetadata(itemIndex);
+	getItemText(itemIndex);
+	getNavigationElements(itemIndex);
+}
 
-// }
+function getItemText(itemIndex) { 
+	let item = currentSelection[itemIndex];
+	const itemTextFile = `${textsDirectory}/${currentTone}_${currentCompetency}/${item.itemText}`;
+
+	fetch(itemTextFile)
+	.then(res => res.text())
+	.then(htmlText => {
+		const tempDiv = document.createElement("div");
+		tempDiv.innerHTML = htmlText;
+
+		const historicalNarrative = tempDiv.querySelector("#historicalNarrative");
+		const hollowKnightNarrative = tempDiv.querySelector("#hollowKnightNarrative");
+		const shortText = tempDiv.querySelector("#shortText");
+		const mediumText = tempDiv.querySelector("#mediumText");
+		const longText = tempDiv.querySelector("#longText");
+
+		if (currentNarrative === "Chronological") {
+			document.getElementById("historicalNarrativeArea").appendChild(historicalNarrative);
+		} else if (currentNarrative === "Hollow Knight") {
+			document.getElementById("hollowKnightNarrativeArea").appendChild(hollowKnightNarrative);
+		} else {
+			console.warn("There is no narrative text specified for this item");
+		}
+
+		const textMap = {
+			"short": shortText,
+			"medium": mediumText,
+			"long": longText
+		};
+
+		const numberOfTextsIncluded = currentTextDurations.length - 1;
+
+		currentTextDurations.forEach((textDuration, i) => {
+			if (i <= numberOfTextsIncluded) {
+				const textArea = document.getElementById(targetAreas[textDuration]);
+				const text = textMap[textDuration]
+				if (textArea && text) {
+					textArea.appendChild(text);
+				}
+			}
+		});
+
+	})
+	.catch(err => {
+		console.error("Error loading content:", err);
+	});
+}
+// when show more is clicked, the text duration is reduced....
+
+function getItemMetadata(itemIndex) {
+	const itemMetadata = currentSelection[itemIndex].metadata;
+	for (const [metadataAttribute, value] of Object.entries(itemMetadata)) {
+		document.getElementById(metadataAttribute).innerHTML = value;
+	}
+}
+
+function getItemImages(itemIndex) { // temporarily just one
+	console.log(currentSelection) // ! fix issue!!! with HK narratives
+	let item = currentSelection[itemIndex];
+	let itemImageArea = document.getElementById("itemImageArea");
+	let hollowKnightImageArea = document.getElementById("hollowKnightImageArea");
+	itemImageArea.src = imageDirectory + "/" + item.itemImages[0];
+	hollowKnightImageArea.src = imageDirectory + "/" + item.hollowKnightImages[0];
+}
+
+function getNavigationElements(itemIndex) {
+	let previousButtonDisabled;
+	let previousButtonMetadata;
+	let nextButtonDisabled;
+	let nextButtonMetadata;
+
+	let metadataAttribute;
+	let currentItemMetadata = currentSelection[itemIndex].metadata;
+
+	if (currentNarrative === "Chronological") {
+		metadataAttribute = "year";
+		document.getElementById("narrativeItem").innerHTML = `Year: ${currentItemMetadata[metadataAttribute]}`
+	} else if (currentNarrative === "Hollow Knight") {
+		metadataAttribute = "hollowKnightArea";
+		document.getElementById("narrativeItem").innerHTML = `Hollow Knight area: ${currentItemMetadata[metadataAttribute]}`
+	} else {
+		console.warn("There are no buttons specified");
+	}
+
+	if (itemIndex > 0) {
+		previousButtonDisabled = false;
+		previousButtonMetadata = currentSelection[itemIndex - 1].metadata[metadataAttribute];
+	} else {
+		previousButtonDisabled = true;
+		previousButtonMetadata = "--";
+	}
+
+	if (itemIndex < currentSelection.length - 1) {
+		nextButtonDisabled = false;
+		nextButtonMetadata = currentSelection[itemIndex + 1].metadata[metadataAttribute];
+	} else {
+		nextButtonDisabled = true
+		nextButtonMetadata = "--"
+	}
+
+	document.getElementById("previousButton").disabled = previousButtonDisabled;
+	document.getElementById("nextButton").disabled = nextButtonDisabled;
+
+	document.getElementById("previousButton").innerHTML = previousButtonMetadata;
+	document.getElementById("nextButton").innerHTML = nextButtonMetadata;
+
+	document.getElementById("cardTitle").innerHTML = currentSelection[itemIndex].shortName
+}
+
+
 
 // ! function : show information 
 // utility functions for showing content should be run in here
